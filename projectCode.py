106e8aee12d5a1561da37e6ebe6558dc4883bef2
Mankala Akhil <150053693+MankalaAkhil@users.noreply.github.com>
2025-04-11 11:15:32 +0530

    Update projectCode.py

--- projectCode.py
+++ projectCode.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import random
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from docx import Document
+import pickle
+import os
+import pandas as pd
+from datetime import datetime
+import pdfplumber
 
